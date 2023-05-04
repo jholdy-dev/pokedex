9bb5b59c365f48a46cd2887b10c176ae3e255d8f
Jholdy Damasceno <jholdydamasceno@gmail.com>
2023-05-04 19:04:11 -0300

    Refactor Vue component and modify Tailwind configuration
    - Remove "HelloWorld.vue" component and related script
    - Update config file to include content attribute and switch darkMode value to "media" to enable dark mode functionality.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false,
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
