d5c39fef1815945ff502723188898a1213a49f5b
Jholdy Damasceno <jholdydamasceno@gmail.com>
2023-05-05 04:57:41 -0300

    feat: Add Pokedex icon to Vue component
    This commit adds a new Vue component called `icon-pokedex.vue` that displays a Pokedex icon. The component uses SVG image imported from the `pokedex.svg` file in the `assets` directory. The icon is bound to the component's template through a reactive variable called `icon`.

---
+++ src/components/icon-pokedex.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Pokedex from "../assets/pokedex.svg";
+import { ref } from "vue";
+
+const icon = ref(Pokedex);
+</script>
+
+<template>
+  <img class="h-12 object m-4" :src="icon" alt="" />
+</template>
