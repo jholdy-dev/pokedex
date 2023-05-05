ee8a9cb5f9c50c755d71583135a5f8611aa60b96
Jholdy Damasceno <jholdydamasceno@gmail.com>
2023-05-05 05:01:42 -0300

    feat: Add pokemon list and detail components
    Add new components to display a list of pokemons, filtered by name, and a detail component that displays the selected pokemon's information. Also added the ability to load more pokemons to the list.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,105 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, onMounted, reactive, ref } from "vue";
+import ListPokemons from "./components/list-pokemons.vue";
+import IconPokedex from "./components/icon-pokedex.vue";
+import PokemonDetail from "./components/pokemon-detail.vue";
+import pokemonRepository, {
+  PokemonDetail as PokemonDetailType,
+} from "./services/pokemon-api";
+
+let pokemons = reactive(ref([] as PokemonDetailType[]));
+let pokemonSelected = ref({} as PokemonDetailType);
+let search = ref("");
+let offset = ref(0);
+let loading = ref(false);
+
+onMounted(async () => {
+  try {
+    pokemons.value = await pokemonRepository.getPokemons(20, offset.value);
+    offset.value += 20;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const pokemonsFiltered = computed(() => {
+  if (pokemons.value && search.value) {
+    return pokemons.value.filter((pokemon: PokemonDetailType) =>
+      pokemon.name.toLowerCase().includes(search.value.toLowerCase())
+    );
+  }
+  return pokemons.value;
+});
+
+const selectPokemon = (pokemonSetter: PokemonDetailType) => {
+  loading.value = true;
+  setTimeout(function () {
+    pokemonSelected.value = pokemonSetter;
+    loading.value = false;
+  }, 500);
+};
+
+const loadMore = async () => {
+  if (offset.value) {
+    loading.value = true;
+    try {
+      const morePokemons = await pokemonRepository.getPokemons(
+        20,
+        offset.value
+      );
+      offset.value += 20;
+      pokemons.value = pokemons.value.concat(morePokemons);
+    } catch (error) {
+      console.log(error);
+    }
+    loading.value = false;
+  }
+};
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 class="text-3xl font-medium text-gray-100 mb-4"><IconPokedex /></h1>
+    <div class="grid md:grid-cols-2 sm:grid-cols-1 gap-4">
+      <div class="grid bg-white p-6 m-6 justify-items-center content-center">
+        <PokemonDetail
+          :name="pokemonSelected.name"
+          :imageUrl="pokemonSelected.imageUrl"
+          :types="pokemonSelected.types"
+          :weight="pokemonSelected.weight"
+          :height="pokemonSelected.height"
+          :loading="loading"
+        />
+      </div>
+      <div class="grid bg-white p-6 m-6 border">
+        <div class="flex items-center border-b border-gray-200 py-2">
+          <input
+            v-model="search"
+            type="text"
+            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none"
+            placeholder="Search"
+          />
+        </div>
+        <div
+          class="overflow-auto min-h-[40rem] max-h-[40rem] grid lg:grid-cols-4 sm:grid-cols-2 gap-4 p-2 m-2"
+        >
+          <ListPokemons
+            v-for="pokemon in pokemonsFiltered"
+            :key="pokemon.id"
+            :name="pokemon.name"
+            :imageUrl="pokemon.imageUrl"
+            :types="pokemon.types"
+            @click="selectPokemon(pokemon)"
+          />
+        </div>
+        <button
+          v-if="offset"
+          @click="loadMore"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Load More
+        </button>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
