f0c9e1410548dd1c0ce8d777b76eda3c811d6308
Jholdy Damasceno <jholdydamasceno@gmail.com>
2023-05-05 04:55:06 -0300

    chore: Add editor config, eslint config, .gitignore, tests, and coverage to package.json
    Add an Editor Config that sets space indentation, trimming trailing whitespace, and final newline insertion. Also add an ESLint config, .gitignore file, and tests with coverage reporting using Vitest to package.json.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const configTestVitest = {
+  test: {
+    environment: "happy-dom",
+  },
+} as any;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  ...configTestVitest,
+});
