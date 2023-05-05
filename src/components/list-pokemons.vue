37f11bc7d253a2c5486e555f35d10acc208f7e4a
Jholdy Damasceno <jholdydamasceno@gmail.com>
2023-05-05 05:00:37 -0300

    feat: Add list-pokemons component
    This commit adds the list-pokemons.vue component which defines a grid displaying the provided Pokemon's image, name and types. The component utilizes the defineProps function to define and render the Pokemon's properties.

---
+++ src/components/list-pokemons.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+const pokemon = defineProps(["imageUrl", "name", "types"]);
+</script>
+
+<template>
+  <div class="h-40 grid bg-white justify-items-center border">
+    <img class="w-28 h-28 object p-2" :src="imageUrl" :alt="pokemon.name" />
+    <div class="text-center">
+      <h1 class="font-bold text-lg mb-1">{{ pokemon.name }}</h1>
+      <span
+        v-for="tp in types"
+        :key="tp"
+        :class="[
+          'px-2.5 py-0.5 rounded mr-2 text-white text-xs font-medium',
+          `bg-${tp.toLowerCase()}`,
+        ]"
+      >
+        {{ tp }}
+      </span>
+    </div>
+  </div>
+</template>
